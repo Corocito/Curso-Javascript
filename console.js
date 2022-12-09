80c3e0648ca184ed36d93e06e741e41ea4ade068
Samuel Orozco <orozcomondragons@gmail.com>
2022-12-09 11:24:56 -0500

    Se añade la explicación de la consola en javascript

---
+++ console.js
@@ -0,0 +1,62 @@
+//-------------------------------------------------------------------------------------------------
+
+
+//assert(): Aparece un mensaje de error en la consola si la afirmacion es falsa.
+//si la afirmacion es verdadera, no aparecerá nada (NO ESTANDAR)
+
+console.assert();
+
+//clear(): Limpia la consola
+
+//error(): Muestra un mensaje de error en la consola web
+
+console.error("Malisimo");
+
+//info(): Emite un mensaje informativo a la consola web, 
+
+//log(): Funciona igual que el info, solo que este se una mas
+
+
+console.log("Lasa gallinas son hermosas");
+
+//table(): Esta funcion toma un argumento obligatorio: data, que debe ser un array
+//o un objeto y un parametro adicional:  nos muestra una tabla en la consola
+
+
+//warn(): Imprime un mensaje de advertencia en la consola web
+
+//dir(): Despliega una lista interactiva de las propiedades del objeto javascript especificado
+
+//-------------------Funciones de conteo
+
+
+//count(): Registra el numero de veces que se llama el count()
+
+//Esta funcion se usa principalmente para saber las veces que se repite un codigo o una funcion
+
+//countReset(): Se resetea el conteo del count
+
+
+
+//------------------------------Funciones de agrupación
+
+
+
+//group(): Crea un nuevo grupo en linea en el registro de la consola web
+
+
+//groupEnd(): Finaliza el grupo creado
+
+
+//groupCollapse(): Tambien crea un grupo, solon que la visualizacion del grupo es cerrada
+
+
+//------------------------------------Funciones de temporizacion
+
+
+//time(): Inicia un temporizador, se puede usar principalmente para timar registros del usuario
+
+//timeEnd(): Finaliza el contador
+
+//timeLog(): Muestra la cantidad de tiempo desde el inicio del contador
+
