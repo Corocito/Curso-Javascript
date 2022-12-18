4f275e989e11aab694d7efdda309d8059f6031df
Samuel Orozco <orozcomondragons@gmail.com>
2022-12-18 14:52:38 -0500

    Se inicia con la explicacion de lo events

---
+++ events.js
@@ -0,0 +1,50 @@
+//--------SE INICIA CON LA SECCIÓN DE LOS EVENTOS DE JAVASCRIPT
+
+/*Eventos del Mouse
+--click: Ocurre con un click
+
+--dbclick: Ocurre con un doble click
+
+--mouseover: Ocurre cuando el puntero se mueve sobre un elemento
+ o sobre uno de sus hijos
+
+ --mouseout: Ocurre cuando el puntero se mueve fuera de un elemento o sus
+ elementos secundarios
+
+
+
+ --------OTROS EVENTOS
+
+ --contextmenu: Ocurre con un click sobre el boton derecho en un elemento
+ para abrir un menu contextual
+
+ --mouseenter: Ocurre cuando el puntero se mueve sobre un elemento
+
+ --mouseleave: Ocurre cuando el puntero se mueve fuera de un elemento
+
+ --mouseup: Ocurre cuando un usuario suelta un boton del mouse sobre un elemento
+
+ --mousemove: Ocurre cuando el puntero se mueve mientras está sobre un elemento
+
+
+
+ ---------EVENTOS DEL TECLADO
+
+ --keypress: Ocurre cuando una tecla se presiona
+
+ --keydown: Ocurre cuando una tecla se deja de presionar
+
+-----------EVENTOS DE LA INTERFAZ
+
+
+--abort: Ocurre cuando un elemento madre elimina a su hijo
+
+--error: Ocurre cuando sucede un error durante la carga de un archivo multimedia
+
+--load: Ocurre cuando un objeto se ha cargado
+
+--beforeunload: Ocurre antes de que el documento este a punto de descargarse
+
+--unload: Ocurre una vez se ha descargado la pagina
+
+*/
