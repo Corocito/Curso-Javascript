59d60de172b439166dd9ef078a2628470ab85c8a
Samuel Orozco <orozcomondragons@gmail.com>
2022-12-19 16:24:30 -0500

    Se termina con la explicación de los eventos

--- events.js
+++ events.js
@@ -1,5 +1,24 @@
 //--------SE INICIA CON LA SECCIÓN DE LOS EVENTOS DE JAVASCRIPT
 
+
+const button = document.querySelector(".button");
+
+//El addEventListener funciona para poder darle la funcionalidad a los eventos
+button.addEventListener("click",(e)=>{
+    alert("Se presiona el botón");
+    e.stopPropagation() //El stopPropagation funciona para que en caso
+    //de tener eventos asociados, se detenga en este
+},true); //El true funciona para que se ejecute primero en caso de haber varios div
+
+
+const button2 = document.querySelector(".button2");
+//los nombres de los eventos se deben de nombrar dentro del parentesis
+button2.addEventListener("dbclick",(e)=>{
+    alert("Se presiona el botón");
+    e.stopPropagation() 
+})
+
+
 /*Eventos del Mouse
 --click: Ocurre con un click
 
@@ -30,14 +49,13 @@
 
  ---------EVENTOS DEL TECLADO
 
- --keypress: Ocurre cuando una tecla se presiona
+ --keypress: Ocurre cuando una tecla se presiona y se suelta
 
- --keydown: Ocurre cuando una tecla se deja de presionar
+ --keydown: Ocurre cuando una tecla se presiona
+
+ --keyup: Ocurre cuando se suelta una tecla
 
 -----------EVENTOS DE LA INTERFAZ
-
-
---abort: Ocurre cuando un elemento madre elimina a su hijo
 
 --error: Ocurre cuando sucede un error durante la carga de un archivo multimedia
 
@@ -47,4 +65,70 @@
 
 --unload: Ocurre una vez se ha descargado la pagina
 
+--resize: Ocurre cuando se cambia el tamaño de la vista del documento
+
+--scroll: Ocurre cuando se deplaza la barra de desplazamiento de un elemento
+
+--select: Ocurre despues de que el usuario selecciona algún texto
 */
+
+
+
+//---------------------EXPLICACIÓN DE EVENTOS DE LA INTERFAZ---------------------------//
+
+//En caso de que ocurra un error, se entrará al codigo y mostrará el error en el codigo
+const imgPrueba= document.querySelector(".imgPrueba");
+imgPrueba.addEventListener("error", ()=>{
+    console.log("ha sucedido un error")
+})
+
+//Con este metodo se manda un mensaje a la consola cuando se termina de cargar la pagina
+window.addEventListener("load",()=>{
+    console.log("Ha cargado la ventana")
+})
+
+
+//Con este metodo se manda un mensaje a la consola antes de que se deje la pagina
+//Este evento es bastante rapido al ejecutarse
+//El evento unload funciona parecido solo que manda el mensaje cuando te sales
+//del sitio Web
+addEventListener("beforeunload",()=>{
+    console.log("Vas a salir del sitio Web")
+    alert("Te vas a ir del sitio")
+})
+
+
+//Con este metodo se manda un mensaje a la consola cada vez que se ajuste
+//la resolucion del sitio Web
+addEventListener("resize",()=>{
+    console.log("Se ha cambiado la resolución del sitio Web")
+})
+
+
+//Scroll es mover la pagina hacia arriba o abajo
+addEventListener("scroll", ()=>{
+    console.log("Se ha scrolleado")
+})
+
+//Select es para cuando se selecciona una parte del texto
+addEventListener("select",(e)=>{
+    console.log("Se ha seleccionado una parte del texto");
+    console.log(e);
+})
+
+//El keyup funciona para poder saber cual fue la ultima tecla presionada
+addEventListener("keyup", (e)=>{
+    let tecla= e.key;
+    nuevoContenido=`La ultima tecla presionada fué: ${tecla}`;
+    console.log(nuevoContenido);
+})
+
+
+//-------------------------EXPLICACIÓN DE LOS TIMERS--------------//
+
+
+
+
+
+
+
