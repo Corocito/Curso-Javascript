dd55b2e48d5ba01e89219d15142c998f1a4c3cae
Samuel Orozco <orozcomondragons@gmail.com>
2022-12-27 16:07:22 -0500

    Se añade la sección de los metodos obsoletos

---
+++ obsoleto.js
@@ -0,0 +1,38 @@
+/*Cuando algo se vuelve obsoleto?
+--"Deprecated": Es la traducción directa de obsoleto, es cuando un metodo no se más ya sea porque se va a reemplazar o porque no se usa más.
+
+--Inútil: Estos metodos son obsoletos ya que perdieron su función, es decir, ya no se usan para nada
+
+--No Recomendado: Estos metodos se vuelven obsoletos en las situaciones en las que por ejemplo existe otro metodo que hace las cosas mejor
+
+--Con Bugs o Fallos
+
+--Está por ser reemplazado
+
+--Hay mejores formas de hacerlo
+
+
+
+------------------------LOS EFECTOS NEGATIVOS(APLICADO A METODOS, CLASES Y PROPIEDADES)
+--Uso excesivo de recursos
+
+--Código con Bugs o Fallos
+
+--Código unnecesariemente largo
+
+--SEO(Optimizacion en los motores de busqueda)
+
+-------------------------COMO VERIFICAR SI ESTÁ OBSOLETO?
+
+--1 de cada 3 WEBS utiliza librerias de JavaScript obsoletas
+
+--Verificar si tienen o usan funciones, métodos, objetos o metodologías obsoletas
+
+--Verificar en los sitios basados en estandares oficiales
+
+w3sxhoolcom(Es una pagina con toda la documentación oficial de distintos lenguajes, para poder saber sobre los metodos actuales)
+
+
+
+
+*/
