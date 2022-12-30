d2aae21e00662989a78991f6d71c42c9cb6ba20d
Samuel Orozco <orozcomondragons@gmail.com>
2022-12-30 17:49:17 -0500

    Se termina con la solucion del Problema de Cofla 12

--- cofla12/script.js
+++ cofla12/script.js
@@ -1,1 +1,49 @@
 //Se inicia con el codigo JavaScript para la solucion del problema de Cofla
+
+//Se define la variable para obtener la información de los objetos de las materias
+const materiasHTML = document.querySelector(".materias");
+
+//Se defina la lista de las notas (Suponiendo que es una base de datos)
+const materias = [
+    {
+        nombre:"Fisica",
+        nota:4
+    },{
+        nombre:"Calculo",
+        nota:5
+    },{
+        nombre:"Bases de Datos",
+        nota:4
+    },{
+        nombre:"Matematicas",
+        nota:3
+    },{
+        nombre:"Programación",
+        nota:4
+    }
+];
+
+let materia=[];
+//Se define el promise con el cual se va a trabajar
+const obtenerMateria = (id)=>{
+    return new Promise((res,rej)=>{
+        materia = materias[id];
+        if(materia == undefined) rej("La materia no existe");
+        else setTimeout(()=>{res(materia)},Math.random()*400);
+    })
+}
+
+
+const devolverMaterias = async()=>{
+    
+    for (let i = 0; i < materias.length; i++) {
+        materia[i] = await obtenerMateria[i];
+        let newHTMLCode = `<div class="materia">
+        <div class="nombre">${materia[i].nombre}</div>
+        <div class="nota">${materia[i].nota}</div>
+        </div>`;
+        materiasHTML.innerHTML += newHTMLCode;
+    }
+}
+
+obtenerMateria()
