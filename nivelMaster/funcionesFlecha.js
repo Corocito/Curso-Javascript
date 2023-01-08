2b7eb8cdff62013dfa3a465df23ddcca704e0813
Samuel Orozco <orozcomondragons@gmail.com>
2023-01-08 14:13:35 -0500

    Se empieza con la explicacion de las funciones flecha

---
+++ nivelMaster/funcionesFlecha.js
@@ -0,0 +1,13 @@
+//------------------SE INICIA CON LA EXPLICACION DE LAS FUNCIONES FLECHA
+ 
+//Las funciones flecha surgieron como una manera simplificar y facilitar
+//el uso de las funciones normales
+
+//Se presenta un ejemplo de como simplificar una funcion con el uso de las funciones flecha
+
+const saludar = function() {return nombre = "Alas"};
+const saludar1 = ()=> nombre = "Blanca";
+
+//AMBAS FUNCIONES HACEN LO MISMO, DEVOLVER UN STRING CON UN NOMBRE, PERO ES MAS
+//FACIL HACIENDO USO DE LAS FUNCIONES FLECHA
+ 
