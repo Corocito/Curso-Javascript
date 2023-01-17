4c24939847e8d6b1fd3968e9a2dc422bdb0ccc27
Samuel Orozco <orozcomondragons@gmail.com>
2023-01-17 16:07:19 -0500

    Se inicia con la explicación de el objeto Date

---
+++ nivelMaster/date.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//SE EMPIEZA CON LA EXPLICACIÓN DE LOS API'S Y COMO SE USAN
+
+//Una API es una interfaz de programación de aplicaciones, cualquier funcionalidad de los
+//lenguajes es una API, es el procesamiento de los resultados, cada lenguaje de programación
+//Es una API ya que desconocemos el proceso interno para poder llegar al resultado esperado
+
+
+
+//-------EXPLICACIÓN DEL OBJETO DATE
+
+//Date es un constructor o una función por lo que se debe de poner entre parentesis para poder llamar a la funcion correctamente
+
+const fechaEjemplo = Date();
+
+console.log(fechaEjemplo);
