067e04a355afaec92929c12998d39037eddf2dd0
Samuel Orozco <orozcomondragons@gmail.com>
2023-01-19 13:40:41 -0500

    Se empieza con la explicación del Storage en JavaScript

---
+++ nivelMaster/storage.js
@@ -0,0 +1,11 @@
+//--------SE INICIA CON LA EXPLICACIÓN DEL LOCAL STORAGE Y EL SESSION STORAGE
+
+
+//El session storage es el almacenamiento que se guarda mientras la pagina este abierta, si se
+//cierra la pestaña la información se va a perder-
+
+//El local storage almacena la información de manera local, por lo que incluso cerrando la pagina
+//la informacion seguiria ahi guardada
+
+
+
