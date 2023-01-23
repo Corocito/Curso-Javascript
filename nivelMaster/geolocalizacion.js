24a3361432a4bccf906a4261767e75f74df432f6
Samuel Orozco <orozcomondragons@gmail.com>
2023-01-23 14:50:39 -0500

    Se añade la explicación inicial de la geolocalización

---
+++ nivelMaster/geolocalizacion.js
@@ -0,0 +1,14 @@
+"use strict";
+//--------------SE EMPIEZA CON LA EXPLICACIÓN DE LA GEOLOCALIZACIÓN
+
+/*
+--getCurrentPosition(): Obtiene todos los datos de la posición actual
+
+-parametros (position, error, options)
+-propiedades de option (enableHighAccuracy, timeout, maximumAge)
+
+--watchPosition():Vigila los cambios en la posición y los muestra
+*/
+
+
+
